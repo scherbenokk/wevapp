c00e986c55cce68adec55a6d3815fb5139daccea
Kirill Scherbenok <k.scherbenok@gmail.com>
2022-09-22 17:11:45 +0200

    added basic blocks for company updates

--- src/components/company-updates/index.js
+++ src/components/company-updates/index.js
@@ -3,9 +3,25 @@ import './index.css';
 function CompanyUpdates() {
   return (
     <div>
-      <h2>Company Updates</h2>
-      <div>
+      <h2 className='header'>Company Updates</h2>
+      <div className='blocks'>
+        <div className='block'>
+          <span>Shares Price</span>
+          <span>201.3</span>
+        </div>
+        <div className='block'>
+          Company Valuation
 
+          500.000.000
+        </div>
+      </div>
+      <div className='blocks'>
+        <div className='block'>
+          New Stakeholders
+        </div>
+        <div className='block'>
+          Cash Flow
+        </div>
       </div>
     </div>
   );
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,9 +1,11 @@
 import './index.css';
+import burger from './burger.svg';
 import CompanyUpdates from '../company-updates';
 
 function Layout() {
   return (
     <div className="container">
+      <img src={burger} className="menu" alt="menu" />
       <CompanyUpdates/>
     </div>
   );
