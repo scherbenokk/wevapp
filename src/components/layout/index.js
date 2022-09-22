3071a3a7b2489ff4012584d9c91abb97a8ad8508
Rutger Cappendijk <rutgercappendijk@gmail.com>
2022-09-22 17:37:33 +0200

    finished personal stats

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,14 @@
 import './index.css';
 import burger from './burger.svg';
 import CompanyUpdates from '../company-updates';
+import PersonalUpdates from '../personal-updates/personal_updates';
 
 function Layout() {
   return (
     <div className="container">
       <img src={burger} className="menu" alt="menu" />
       <CompanyUpdates/>
+      <PersonalUpdates />
     </div>
   );
 }
